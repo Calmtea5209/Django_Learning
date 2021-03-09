cb6d0bba21b0e6344ded422ea368d004c3e9348a
Calmtea <willy5209593@gmail.com>
2021-03-09 11:19:05 +0800

    利用templates語法建立基本網站顯示

--- mainsite/models.py
+++ mainsite/models.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=100)
     body = models.TextField()
     pub_date = models.DateTimeField(default=timezone.now)
     class Meta:
-        ordering = ('-pub_date',) #文章顯示依時間顯示
+        ordering = ('-pub_date',) #文章顯示依時間依序顯示
     def __str__(self):
         return self.title
 # Create your models here.
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
 def homepage(request):
     return render(request,"main.html")
 def article(request):
     posts = Post.objects.all()
-    post_lists = list()
-    for count,post in enumerate(posts):
-        post_lists.append("No.{} : ".format(str(count+1)) + str(post) +"<hr>")
-        post_lists.append("<small>" + str(post.body) + "</small><br><br>") 
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    return render(request,"article.html",locals())
 # Create your views here.
